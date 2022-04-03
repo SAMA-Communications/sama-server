62456f2f00aa21ca3837a30dc0d3bb9f6e35abb6
IgorKhomenko <khomenkoigor+1@gmail.com>
2022-04-03 21:11:51 +0300

    client: login/signup forms

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,40 @@ import React, { useState, useEffect } from 'react';
 
 import Main from './components/Main'
 import Login from './components/Login'
+import SignUp from './components/SignUp'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoginView, setIsLoginView] = useState(false);
+  const [isCreateUserView, setIsCreateUserView] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
-    setIsLoggedIn(!!token);
+    setIsLoginView(!(!!token));
   }, []);
 
-  if (!isLoggedIn) {
-    return <Login />;
+  const onLogin = () => {
+
+  };
+
+  const onSignUp = () => {
+
+  };
+
+  const onDisplaySignUp = () => {
+    setIsCreateUserView(true)
+    setIsLoginView(false)
+  }
+
+  const onDisplayLogin = () => {
+    setIsCreateUserView(false)
+    setIsLoginView(true)
+  }
+
+  if (isLoginView) {
+    return <Login onSubmit={onLogin} onSignUp={onDisplaySignUp}/>;
+  }
+  if (isCreateUserView) {
+    return <SignUp onSubmit={onSignUp} onLogin={onDisplayLogin}/>;
   }
 
   return <Main />;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,45 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react';
+import React from 'react';
 
-export default function Login() {
+import '../styles/Login.css';
+
+export default function Login({ onSubmit, onSignUp, error }) {
+  const handleSubmit = (event) => {
+    // Prevent page reload
+    event.preventDefault();
+
+    onSubmit();
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = () =>
+    <div className="error">{error}</div>;
+
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {error && renderErrorMessage()}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+        <a className="signup" onClick={onSignUp}>Create user</a>
+      </form>
+    </div>
+  );
+
   return (
-    <p>
-     Login form
-    </p>
+    <div className="login-container">
+      <div className="login-form">
+        <div className="title">Sign In</div>
+        {renderForm}
+      </div>
+    </div>
   );
 }
