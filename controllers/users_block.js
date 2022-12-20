5ace641c1bffd8d5c68d1b2827e8428be781fdb8
Oleksandr <olexander11441144@gmail.com>
2022-12-20 14:11:30 +0200

    UserBlockController -> unblock

--- controllers/users_block.js
+++ controllers/users_block.js
@@ -1,3 +1,4 @@
+import { ERROR_STATUES } from "../constants/http_constants.js";
 import validate, {
   validateIsConversationByCID,
   validateIsUserId,
@@ -7,9 +8,8 @@ import BlockedUser from "../models/blocked_user.js";
 export default class UserBlockController {
   async block(ws, data) {
     const requestId = data.request.id;
-    const uId = data.request.user_id;
+    const uId = data.request.id;
     const convId = data.request.cid;
-
     await validate(ws, { cid: convId, uId }, [
       validateIsConversationByCID,
       validateIsUserId,
@@ -30,7 +30,18 @@ export default class UserBlockController {
 
   async unblock(ws, data) {
     const requestId = data.request.id;
-    return { response: { id: requestId, success: true } };
+    const uId = data.request.id;
+    await validate(ws, { uId }, [validateIsUserId]);
+
+    const record = await BlockedUser.findOne({ user_id: uId });
+    if (record) {
+      await record.delete();
+      return { response: { id: requestId, success: true } };
+    } else {
+      throw new Error(ERROR_STATUES.USER_NOT_BLOCKED.message, {
+        cause: ERROR_STATUES.USER_NOT_BLOCKED,
+      });
+    }
   }
 
   async list(ws, data) {
