01b68a7e4575a2d0abaad5d161bca02a47ad7cb4
Oleksandr <olexander11441144@gmail.com>
2023-01-10 17:03:49 +0200

    create new indexes

---
+++ migrations/20230110145515-set-user-login.js
@@ -0,0 +1,5 @@
+export const up = async (db, client) => {
+  await db.collection("users").createIndex({ login: 1 });
+};
+
+export const down = async (db, client) => {};
---
+++ migrations/20230110150121-set-message-status.js
@@ -0,0 +1,5 @@
+export const up = async (db, client) => {
+  await db.collection("message_status").createIndex({ mid: 1 });
+};
+
+export const down = async (db, client) => {};
