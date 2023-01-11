0a2bfff8153ef3f87721a2d066e17a92f480aa7f
Oleksandr <olexander11441144@gmail.com>
2023-01-11 17:56:01 +0200

    set-user-token index update

--- migrations/20221018131921-set-user-token-tt1.js
+++ migrations/20221018131921-set-user-token-tt1.js
@@ -3,7 +3,7 @@ export const up = async (db, client) => {
     .collection("user_token")
     .createIndex(
       { updated_at: 1 },
-      { expireAfterSeconds: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN }
+      { expireAfterSeconds: parseInt(process.env.JWT_ACCESS_TOKEN_EXPIRES_IN) }
     );
   await db.collection("user_token").createIndex({ token: 1, device_id: 1 });
   await db.collection("user_token").createIndex({ user_id: 1, device_id: 1 });
