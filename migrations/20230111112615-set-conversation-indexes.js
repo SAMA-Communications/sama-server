b3473f0e75ef06c394713499e2f1e2c9a0a4e0cc
Oleksandr <olexander11441144@gmail.com>
2023-01-12 15:48:03 +0200

    update index

--- migrations/20230111112615-set-conversation-indexes.js
+++ migrations/20230111112615-set-conversation-indexes.js
@@ -1,13 +1,13 @@
 export const up = async (db, client) => {
   await db
     .collection("conversations")
-    .createIndex({ owner_id: 1, opponent_id: 1, type: 1 });
+    .createIndex({ type: 1, owner_id: 1, opponent_id: 1 });
   await db.collection("conversations").createIndex({ updated_at: 1 });
 };
 
 export const down = async (db, client) => {
   await db
     .collection("conversations")
-    .dropIndex({ owner_id: 1, opponent_id: 1, type: 1 });
+    .dropIndex({ type: 1, owner_id: 1, opponent_id: 1 });
   await db.collection("conversations").dropIndex({ updated_at: 1 });
 };
