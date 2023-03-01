ce2dd24fe1d03ef89a8fc73e3c7417727bbec98b
Oleksandr <olexander11441144@gmail.com>
2023-03-01 11:27:00 +0200

    remove async from validate

--- app/validations/validation.js
+++ app/validations/validation.js
@@ -1,7 +1,22 @@
+import Joi from "joi";
+
 export default class Validation {
-  constructor() {}
+  constructor() {
+    this.shem;
+  }
 
   validate() {
+    const shema = Joi.object({
+      login: Joi.string().min(10).max(30).required(),
+      password: Joi.string().required(),
+      deviceId: Joi.number(),
+    });
+    const res = shema.validate({
+      login: "123",
+      password: "123",
+      deviceId: 123412,
+    });
+    console.log("result:", res);
     console.log("Validate working!");
     return this;
   }
