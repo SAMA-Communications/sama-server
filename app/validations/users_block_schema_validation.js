76f3b43357af104681c85b743a6c6e9332353fd1
Oleksandr <olexander11441144@gmail.com>
2023-03-03 01:25:29 +0200

    fixed users_block schema

--- app/validations/users_block_schema_validation.js
+++ app/validations/users_block_schema_validation.js
@@ -1,11 +1,24 @@
 import Joi from "joi";
+import { ERROR_STATUES } from "./../constants/http_constants.js";
 
 export const usersBlockSchemaValidation = {
   block: Joi.object({
-    id: Joi.string().required(),
-  }).required(),
+    id: Joi.alternatives().try(Joi.object(), Joi.string()).required(),
+  })
+    .required()
+    .error(
+      new Error(ERROR_STATUES.USER_ID_MISSED.message, {
+        cause: ERROR_STATUES.USER_ID_MISSED,
+      })
+    ),
   unblock: Joi.object({
-    id: Joi.string().required(),
-  }).required(),
+    id: Joi.alternatives().try(Joi.object(), Joi.string()).required(),
+  })
+    .required()
+    .error(
+      new Error(ERROR_STATUES.USER_ID_MISSED.message, {
+        cause: ERROR_STATUES.USER_ID_MISSED,
+      })
+    ),
   list: Joi.object({}).required(),
 };
