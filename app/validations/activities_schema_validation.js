6c95467703dec797348e78f23f87445ea45a4bfc
Oleksandr <olexander11441144@gmail.com>
2023-03-03 11:46:38 +0200

    fixed activities schema

--- app/validations/activities_schema_validation.js
+++ app/validations/activities_schema_validation.js
@@ -1,11 +1,21 @@
 import Joi from "joi";
+import { ERROR_STATUES } from "./../constants/http_constants.js";
 
 export const activitiesSchemaValidation = {
   statusSubscribe: Joi.object({
-    id: Joi.string().required(),
+    id: Joi.alternatives()
+      .try(Joi.object(), Joi.string())
+      .required()
+      .error(
+        new Error(ERROR_STATUES.USER_ID_MISSED.message, {
+          cause: ERROR_STATUES.USER_ID_MISSED,
+        })
+      ),
   }).required(),
   statusUnsubscribe: Joi.object({}).required(),
   getUserStatus: Joi.object({
-    ids: Joi.array().items(Joi.string().required()).required(),
+    ids: Joi.array()
+      .items(Joi.alternatives().try(Joi.object(), Joi.string()).required())
+      .required(),
   }).required(),
 };
