7ed17a039aa7b0d17cc5d35b468c489b1efa93d5
Oleksandr <olexander11441144@gmail.com>
2023-04-05 10:07:20 +0300

    update migrations

--- migrations/20230403104717-set-user-indexes.js
+++ migrations/20230403104717-set-user-indexes.js
@@ -6,12 +6,10 @@ export const up = async (db, client) => {
   await db
     .collection("users")
     .createIndex({ phone: 1 }, { unique: true, sparse: true });
-  await db.collection("users").createIndex({ login: 1, updated_at: 1 });
 };
 
 export const down = async (db, client) => {
   await db.collection("users").dropIndex({ login: 1 });
   await db.collection("users").dropIndex({ email: 1 });
   await db.collection("users").dropIndex({ phone: 1 });
-  await db.collection("users").dropIndex({ login: 1, updated_at: 1 });
 };
