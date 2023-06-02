b308bb0f80602a5eed7dfa77fadc27bd3ee2be8e
Oleksandr <olexander11441144@gmail.com>
2023-06-02 09:48:03 +0300

    return bull queue options

--- app/queues/notification_queue.js
+++ app/queues/notification_queue.js
@@ -2,5 +2,8 @@ import Queue from "bull";
 
 export const pushNotificationQueue = new Queue(
   "notification",
-  process.env.REDIS_URL
+  process.env.REDIS_URL,
+  {
+    redis: { maxRetriesPerRequest: null, enableReadyCheck: false },
+  }
 );
