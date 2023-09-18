dc31078b4c0f6efb528d7b383da18c2168e552f4
Oleksandr <98953475+Oleksandr1414@users.noreply.github.com>
2023-09-19 00:07:02 +0300

    add update-message-status migration (#66)

---
+++ migrations/20230906093933-update-message-status.js
@@ -0,0 +1,13 @@
+export const up = async (db, client) => {
+  await db.collection("message_status").dropIndex({ mid: 1 });
+  await db
+    .collection("message_status")
+    .createIndex({ cid: 1, mid: 1, user_id: 1 }, { unique: true });
+};
+
+export const down = async (db, client) => {
+  await db.collection("message_status").dropIndex({ mid: 1 });
+  await db
+    .collection("message_status")
+    .dropIndex({ cid: 1, mid: 1, user_id: 1 });
+};
