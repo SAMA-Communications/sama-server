c6b569495a0f3d1055d9309cc82ebd09d882bb52
Oleksandr <olexander11441144@gmail.com>
2024-01-04 11:31:26 +0200

    SAMA-69 add private method for key generation

--- app/repositories/file_repository.js
+++ app/repositories/file_repository.js
@@ -6,12 +6,16 @@ export default class FileRepository extends BaseRepository {
     super(null, inMemoryStorage);
   }
 
+  #createKey(fileId) {
+    return `file:${fileId}`;
+  }
+
   async getFileUrl(fileId) {
     return (await RedisClient.client.sMembers(`file:${fileId}`))[0];
   }
 
   async storeFileUrl(fileId, url) {
-    await RedisClient.client.sAdd(`file:${fileId}`, url);
-    await RedisClient.client.expire(`file:${fileId}`, 3600);
+    await RedisClient.client.sAdd(this.createKey(fileId), url);
+    await RedisClient.client.expire(this.createKey(fileId), 3600);
   }
 }
