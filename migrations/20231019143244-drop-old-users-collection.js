2421d61cdb7fa3fc5e9c2d500648ac0acd069863
Oleksandr <olexander11441144@gmail.com>
2024-01-11 12:44:35 +0200

    delete migrations and rename spaces endpoint variable

--- migrations/20231019143244-drop-old-users-collection.js
+++
@@ -1,7 +0,0 @@
-export const up = async (db, client) => {
-  await db.collection("old_users").drop();
-};
-
-export const down = async (db, client) => {
-  await db.createCollection("old_users");
-};
