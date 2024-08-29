f254668fbfe1ae1fc75fa7f43fd5938a9f1d4240
Oleksandr <olexander11441144@gmail.com>
2024-08-29 15:55:38 +0300

    SK-248 added `is_encrypted` field for conversation object

--- app/providers/repositories/encryption/index.js
+++ app/providers/repositories/encryption/index.js
@@ -56,7 +56,7 @@ class EncryptionRepository extends BaseRepository {
         (result[obj._id] = obj.devices.map((device) => ({
           identity_key: device.identity_key,
           signed_key: device.signed_key,
-          one_time_pre_keys: device.one_time_pre_keys.slice(0, 1), //{ $arrayElemAt: ["$one_time_pre_keys", 0] } }
+          one_time_pre_keys: Object.values(device.one_time_pre_keys)[0],
         })))
     )
 
