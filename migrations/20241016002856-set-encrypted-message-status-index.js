0adfffb0c9b825639c653bf622c76e5d86c88e40
Oleksandr Chabaniuk <olexander11441144@gmail.com>
2024-10-16 14:39:31 +0300

    SK-263 updated `set-encrypted-message-ttl`

--- migrations/20241016002856-set-encrypted-message-status-index.js
+++ migrations/20241016002856-set-encrypted-message-status-index.js
@@ -1,11 +1,11 @@
 export const up = async (db, client) => {
-  await db.collection("encrypted_message_status").createIndex({ mid: 1 }, { uniqe: true })
+  await db.collection("encrypted_message_statuses").createIndex({ mid: 1 }, { uniqe: true })
   await db
-    .collection("encrypted_message_status")
+    .collection("encrypted_message_statuses")
     .createIndex({ created_at: 1 }, { expireAfterSeconds: parseInt(process.env.ENCRYPTION_MESSAGE_EXPIRED_IN) })
 }
 
 export const down = async (db, client) => {
-  await db.collection("encrypted_message_status").dropIndex({ mid: 1 })
-  await db.collection("encrypted_message_status").dropIndex({ created_at: 1 })
+  await db.collection("encrypted_message_statuses").dropIndex({ mid: 1 })
+  await db.collection("encrypted_message_statuses").dropIndex({ created_at: 1 })
 }
