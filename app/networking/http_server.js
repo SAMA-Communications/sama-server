0317fb9ecc5b5b91900c431eaaefc6135a840ed9
AntonSelezev <anton@connectycube.com>
2025-03-31 18:29:42 +0300

    fix parseJsonBodyMiddleware

--- app/networking/http_server.js
+++ app/networking/http_server.js
@@ -60,7 +60,7 @@ const parseJsonBodyMiddleware = async (res, req) => {
   const contentType = res.parsedHeaders["content-type"]
   const contentLength = +res.parsedHeaders["content-length"]
 
-  if (contentType !== "application/json" || !contentLength) {
+  if (contentType?.includes("application/json") || !contentLength) {
     return
   }
 
