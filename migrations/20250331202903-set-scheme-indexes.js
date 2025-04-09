073bcd80c45130bd8a9cb230e13a58ed81ba47ea
Oleksandr Chabaniuk <olexander11441144@gmail.com>
2025-04-09 11:53:07 +0300

    fix last migration 2/2

--- migrations/20250331202903-set-scheme-indexes.js
+++ migrations/20250331202903-set-scheme-indexes.js
@@ -1,7 +1,7 @@
 export const up = async (db, client) => {
-  await db.collection("conversation_schemes").createIndex({ cid: 1 }, { unique: true })
+  await db.collection("conversation_schemes").createIndex({ conversation_id: 1 }, { unique: true })
 }
 
 export const down = async (db, client) => {
-  await db.collection("conversation_schemes").dropIndex({ cid: 1 })
+  await db.collection("conversation_schemes").dropIndex({ conversation_id: 1 })
 }
