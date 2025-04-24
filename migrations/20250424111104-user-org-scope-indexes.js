86d2136db4b16eceddfdfa2e72ebbe9a1c6d7a10
AntonSelezev <anton@connectycube.com>
2025-04-24 16:34:39 +0300

    user indexes: remove sparse, add partialFilterExpression

--- migrations/20250424111104-user-org-scope-indexes.js
+++ migrations/20250424111104-user-org-scope-indexes.js
@@ -6,8 +6,8 @@ export const up = async (db, client) => {
 
     await db.collection("users").createIndex({ organization_id: 1, login: 1, updated_at: 1 })
     await db.collection("users").createIndex({ organization_id: 1, login: 1 }, { unique: true })
-    await db.collection("users").createIndex({ organization_id: 1, email: 1 }, { unique: true, sparse: true })
-    await db.collection("users").createIndex({ organization_id: 1, phone: 1 }, { unique: true, sparse: true })
+    await db.collection("users").createIndex({ organization_id: 1, email: 1 }, { unique: true, partialFilterExpression: { email: { $gt: '' } } })
+    await db.collection("users").createIndex({ organization_id: 1, phone: 1 }, { unique: true, partialFilterExpression: { phone: { $gt: '' } } })
   }
   
   export const down = async (db, client) => {
