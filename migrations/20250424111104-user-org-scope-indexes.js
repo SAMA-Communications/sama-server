071c80b9d518c3bd8717b2c18523c32551603de1
AntonSelezev <anton@connectycube.com>
2025-04-24 15:38:35 +0300

    add sparse

--- migrations/20250424111104-user-org-scope-indexes.js
+++ migrations/20250424111104-user-org-scope-indexes.js
@@ -4,10 +4,10 @@ export const up = async (db, client) => {
     await db.collection("users").dropIndex({ email: 1 })
     await db.collection("users").dropIndex({ phone: 1 })
 
-    await db.collection("users").createIndex({ organization_id: 1, login: 1, updated_at: 1 }, { collation: { locale: "en", strength: 2 } })
-    await db.collection("users").createIndex({ organization_id: 1, login: 1 }, { unique: true, collation: { locale: "en", strength: 2 } })
-    await db.collection("users").createIndex({ organization_id: 1, email: 1 }, { unique: true, collation: { locale: "en", strength: 2 } })
-    await db.collection("users").createIndex({ organization_id: 1, phone: 1 }, { unique: true, collation: { locale: "en", strength: 2 } })
+    await db.collection("users").createIndex({ organization_id: 1, login: 1, updated_at: 1 })
+    await db.collection("users").createIndex({ organization_id: 1, login: 1 }, { unique: true })
+    await db.collection("users").createIndex({ organization_id: 1, email: 1 }, { unique: true, sparse: true })
+    await db.collection("users").createIndex({ organization_id: 1, phone: 1 }, { unique: true, sparse: true })
   }
   
   export const down = async (db, client) => {
@@ -16,9 +16,9 @@ export const up = async (db, client) => {
     await db.collection("users").dropIndex({ organization_id: 1, email: 1 })
     await db.collection("users").dropIndex({ organization_id: 1, phone: 1 })
 
-    await db.collection("users").createIndex({ login: 1, updated_at: 1 }, { collation: { locale: "en", strength: 2 } })
-    await db.collection("users").createIndex({ login: 1 }, { unique: true, collation: { locale: "en", strength: 2 } })
-    await db.collection("users").createIndex({ email: 1 }, { unique: true, collation: { locale: "en", strength: 2 } })
-    await db.collection("users").createIndex({ phone: 1 }, { unique: true, collation: { locale: "en", strength: 2 } })
+    await db.collection("users").createIndex({ login: 1, updated_at: 1 })
+    await db.collection("users").createIndex({ login: 1 }, { unique: true })
+    await db.collection("users").createIndex({ email: 1 }, { unique: true, sparse: true })
+    await db.collection("users").createIndex({ phone: 1 }, { unique: true, sparse: true })
   }
   
