322b83ea4008f6b4636ae8f5c7189f4bdf00beac
Oleksandr <98953475+Oleksandr1414@users.noreply.github.com>
2025-06-16 10:40:31 +0300

    Extended `Message` object with `replied_message_id` field (#183)

--- test/messages.spec.js
+++ test/messages.spec.js
@@ -69,8 +69,58 @@ describe("Message function", async () => {
         }
       }
 
+      messageId1 = responseData.ask.server_mid
+
       assert.strictEqual("xyz", responseData.ask.mid)
       assert.notEqual(responseData.ask.t, undefined)
+    })
+
+    it("should work with reply id", async () => {
+      const requestData = {
+        message: {
+          id: "xyzd",
+          body: "hey how is going?",
+          cid: currentConversationId,
+          replied_message_id: messageId1,
+          x: {
+            param1: "value",
+            param2: "value",
+          },
+        },
+      }
+      let responseData = null
+
+      responseData = await packetJsonProcessor.processMessageOrError(mockedWS, JSON.stringify(requestData))
+
+      responseData = responseData.backMessages.at(0)
+
+      assert.strictEqual("xyzd", responseData.ask.mid)
+      assert.notEqual(responseData.ask.t, undefined)
+    })
+
+    it("should fail incorrect reply message id", async () => {
+      const requestData = {
+        message: {
+          id: "xyzda",
+          body: "hey how is going?",
+          cid: currentConversationId,
+          replied_message_id: "123",
+          x: {
+            param1: "value",
+            param2: "value",
+          },
+        },
+      }
+
+      let responseData = await packetJsonProcessor.processMessageOrError(mockedWS, JSON.stringify(requestData))
+
+      responseData = responseData.backMessages.at(0)
+
+      assert.equal(responseData.ask, undefined)
+      assert.deepEqual(responseData.message.error, {
+        status: 422,
+        message: "Incorrect reply message ID.",
+      })
     })
 
     it("should fail incorrect ID", async () => {
